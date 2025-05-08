d72dea177da264d347d29fd9f068c2aac7d872d9
BreimarTejada <btejada@ucundinamarca.edu.co>
2025-05-07 19:46:26 -0500

    Mejoras en el codigo

--- Taller7/contact.js
+++ Taller7/contact.js
@@ -1,15 +1,21 @@
-// JS
 function enviar() {
-        const email = document.getElementById('email');
-        const name = document.getElementById('name');
-        const msg = document.getElementById('msg');
-    
-        if (!email.checkValidity() || !name.value.trim() || !msg.value.trim()) {
-            alert("Por favor, completa todos los campos correctamente.");
-            return;
-        }
-    
-        alert("Gracias por contactarnos, " + name.value);
-        msg.textContent = "Mensaje enviado correctamente.";
+    const email = document.getElementById('email');
+    const name = document.getElementById('name');
+    const msg = document.getElementById('msg');
+    const status = document.getElementById('status');
+
+    if (!email.checkValidity() || !name.value.trim() || !msg.value.trim()) {
+        alert("Por favor, completa todos los campos correctamente.");
+        return;
     }
-    
+
+    alert("Gracias por contactarnos, " + name.value);
+
+    // Limpiar campos
+    msg.value = "";
+    name.value = "";
+    email.value = "";
+
+    // Mostrar mensaje de confirmación
+    status.textContent = "Mensaje enviado correctamente.";
+}
